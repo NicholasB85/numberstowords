a8d30555284c3e373cde87a4ed8961020f8ccc09
NicholasB85 <nicholas.w.burkhart@gmail.com>
2018-05-02 11:17:44 -0400

    added line break

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ const ones = ["one","two","three","four","five","six","seven","eight","nine",];
 const teen = [" ","eleven","twelve","thirteen","fourteen","fifteen","sixteen","seventeen","eighteen","nineteen"];
 const tens = [" ","ten","twenty","thirty","fourty","fifty","sixty","seventy","eighty","ninety"];
 const hundreds = [" ","one hundred", "two hundred","three hundred","four hundred","five hundred","six hundred","seven hundred","eight hundred","nine hundred"];
-const thous =[" ","thousand"];
+const thous =[" ","one thousand","two thousand","three thousand","four thousand","five thousand","six thousand","seven thousand","eight thousand","nine thousand"];
 
 function convert(){
     let numbers = " ";
@@ -12,9 +12,9 @@ function convert(){
         for(let t = 0; t < tens.length; t++) {
             for(let o = 0; o < ones.length; o++){
                if(t===1 && o !==0){
-                   numbers +=hundreds[h] +" "+teen[o] + " ";
+                   numbers +=hundreds[h] +" "+teen[o] + " " + "<br>";
                }else
-                numbers += " "+ thous[th]+" " + hundreds[h] + " " + tens[t] + " " + ones[o];
+                numbers += " "+ thous[th]+" " + hundreds[h] + " " + tens[t] + " " + ones[o]+ "<br>";
             }
         }
     }
