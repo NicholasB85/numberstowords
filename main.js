607caf6163989aa0b2e76e4ce73d2a1bda8c217c
NicholasB85 <nicholas.w.burkhart@gmail.com>
2018-08-08 13:59:03 -0400

    added the ability to covert up to ten thousand

--- main.js
+++ main.js
@@ -3,20 +3,21 @@ const ones = [" ", "one","two","three","four","five","six","seven","eight","nine
 const teen = [" ","eleven","twelve","thirteen","fourteen","fifteen","sixteen","seventeen","eighteen","nineteen"];
 const tens = [" ","ten","twenty","thirty","fourty","fifty","sixty","seventy","eighty","ninety"];
 const hundreds = [" ","one hundred", "two hundred","three hundred","four hundred","five hundred","six hundred","seven hundred","eight hundred","nine hundred"];
-const thous =[" ","one thousand","two thousand","three thousand","four thousand","five thousand","six thousand","seven thousand","eight thousand","nine thousand"];
+const thous =[" ", "one thousand","two thousand","three thousand","four thousand","five thousand","six thousand","seven thousand","eight thousand","nine thousand"];
 
 function convert(){
     let numbers = " ";
-    
+    for (let th = 0; th < thous.length; th++){
     for (let h = 0; h < hundreds.length; h++) {
         for(let t = 0; t < tens.length; t++) {
             for(let o = 0; o < ones.length; o++){
                if(t===1 && o !==0){
-                   numbers += hundreds[h] + " " +teen[o] + " " + "<br>";
+                   numbers +=thous[th] + " " + hundreds[h] + " " +teen[o] + " " + "<br>";
                }else
-                numbers +=  " " + hundreds[h] + " " + tens[t] + " " + ones[o]+ "<br>";
+                numbers += thous[th] + " " + hundreds[h] + " " + tens[t] + " " + ones[o]+ "<br>";
             }
         }
+    }
     
     
 }
